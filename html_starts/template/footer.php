a0af087ee0ac3a5f2faa051e63675e4a35a5f96d
Gyamie <106616470+Gyamie@users.noreply.github.com>
2022-07-10 22:35:07 +0000

    Add files via upload

---
+++ html_starts/template/footer.php
@@ -0,0 +1,8 @@
+<br>  <br> <br>
+  <footer>
+      <div class="f_container"> 
+  <p>Copyright © 2022 Kidadl Ltd. All Rights Reserved.Update Privacy SettingsExclusive Member of Mediavine Family.</p>
+</div>
+</footer>
+</body>
+</html>
